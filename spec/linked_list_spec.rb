06296c114c5c238bf6954e2e77d380dc0f42a68e
Justin Pasko <jpasko1112@gmail.com>
2023-03-23 22:17:31 -0400

    add linked_list.rb and linked_list_spec.rb files.

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe LinkedList do
+
+  it "exists" do
+    list = LinkedList.new
+  end
+
+  it "has a head" do
+    list = LinkedList.new
+    
+  end
+    
+end
