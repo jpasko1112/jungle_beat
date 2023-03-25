40170070b286ef24d81e5a1bb631e03c72a3824f
Justin Pasko <jpasko1112@gmail.com>
2023-03-25 12:30:35 -0400

    Add changes to Node class and clean up code display.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,7 +5,43 @@ class LinkedList
     @head = nil
   end
 
-  def apppend(value)
-    
+  def append(data)
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      current_node = @head
+      new_node = Node.new(data)
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+        current_node.next_node = new_node
+    end
+  end
+
+  def count
+    if @head.nil?
+      counter = 0
+    else
+      counter = 1
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def to_string(data)
+    string = ""
+    current_node = @head
+    if @head.nil?
+      return string
+    else
+      while current_node.next != nil
+        string << current_node.data
+      end
+      string
+    end
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,13 +2,16 @@ require "./lib/linked_list"
 require "./lib/node"
 
 RSpec.describe LinkedList do
-
+  
   it "exists" do
     list = LinkedList.new
+
+    expect(list).to be_an_instance_of(LinkedList)
   end
 
   it "has a head" do
     list = LinkedList.new
+
     expect(list.head).to eq(nil)
   end
 
@@ -18,8 +21,37 @@ RSpec.describe LinkedList do
     expect(list.head).to eq(nil)
 
     list.append("doop")
+    
+    expect(list.head.data).to eq("doop")
+require 'pry'; binding.pry
+    expect(list.head.next_node).to eq(nil)
+  end
+ 
 
-    expect(list.head).to eq("doop")
+  it "can check next node" do
+    list = LinkedList.new
+    expect(list.head).to eq(nil)
+
+    list.append("doop")
+
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node).to eq(nil)
+
+  end
+
+  it "can count nodes" do
+    list = LinkedList.new
+    expect(list.head).to eq(nil)
+
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
   end
     
+  xit "can be a string" do
+    list = LinkedList.new
+    expect(list.head).to eq(nil)
+    list.append("doop")
+    list.to_string
+    expect(list.to_string).to eq ("doop")
+  end
 end
