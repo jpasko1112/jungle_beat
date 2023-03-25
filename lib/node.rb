a7f6c61e2ed8e41bedc84da525a802882605a607
Justin Pasko <jpasko1112@gmail.com>
2023-03-25 11:47:15 -0400

    Add changes to lib/node and lib.node_spec files

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,9 @@
 class Node
 
-    attr_reader :data
+    attr_reader :data, :next_node
   def initialize(data)
     @data = data
+    @next_node = next_node
   end
-
-  def next_node
-    ()
-  end
-
 
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,11 +7,4 @@ RSpec.describe Node do
     expect(node).to be_an_instance_of(Node)
   end
 
-  it "has data" do
-    node = Node.new("plop")
-
-    expect(node.data).to eq("plop")
-    expect(node.next_node).to eq(nil)
-  end
-
 end
