323566cf95f55662c82b98a7e52a4b6b56f6f0ec
Justin Pasko <jpasko1112@gmail.com>
2023-03-26 13:49:05 -0400

    Add Find and Includes? tests in the spec file.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -80,10 +80,46 @@ RSpec.describe LinkedList do
     list.append("plop")
     list.append("suu")
     list.prepend("dop")
-
     list.insert(1, "woo")
     list.to_string
 
     expect(list.to_string).to eq("dop woo plop suu")
+  end
+
+  # Find - takes two parameters (index, how many)
+  it "can find" do
+    list = LinkedList.new
+
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.to_string).to eq("deep woo shi shu blop")
+
+    expect(list.find(2, 1)).to eq("shi")
+    expect(list.find(1, 3)).to eq("woo shi shu")
+  end
+
+  # Includes? gives back true or false whether the supplied value is in the list.
+  it "checks for inclusion" do
+    list = LinkedList.new
+
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.to_string).to eq("deep woo shi shu blop")
+
+    list.includes?("deep")
+
+    expect(list.includes?("deep")).to be(true)
+
+    list.includes?("dep")
+
+    expect(list.includes?("dep")).to be(false)
   end
 end
