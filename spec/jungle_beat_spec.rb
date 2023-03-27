1233e3332cf09448a7d00371befc971d6f3ee121
Justin Pasko <jpasko1112@gmail.com>
2023-03-27 16:22:48 -0400

    Refactor 'append' test to include more expect statements.

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe JungleBeat do
     jb = JungleBeat.new
 
     expect(jb).to be_an_instance_of(JungleBeat)
+  end
+
+  it "has readable attributes" do
+    jb = JungleBeat.new
+
     expect(jb.list.head).to eq(nil)
   end
 
@@ -14,5 +19,7 @@ RSpec.describe JungleBeat do
     jb = JungleBeat.new
 
     expect(jb.append("deep doo ditt")).to eq("deep doo ditt")
+    expect(jb.list.head.data).to eq("deep")
+    expect(jb.list.head.next.data).to eq("ditt")
   end
 end
