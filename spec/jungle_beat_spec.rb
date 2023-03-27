8cb3065fbbef2f84fb67f8bcc4145412464764bc
Justin Pasko <jpasko1112@gmail.com>
2023-03-26 23:00:03 -0400

    test: add test for 'exists', test: add test for append

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe JungleBeat do
   it "exists" do
     jb = JungleBeat.new
 
-    expect(jb).to_be_an_instance_of(JungleBeat)
+    expect(jb).to be_an_instance_of(JungleBeat)
+    expect(jb.list.head).to eq(nil)
+  end
+
+  it "will append" do
+    jb = JungleBeat.new
+
+    expect(jb.append("deep doo ditt")).to eq("deep doo ditt")
   end
 end
